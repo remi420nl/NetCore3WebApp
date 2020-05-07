ac1ef923e5daab417714ddcbc0fa750692647d7c
Remi Peerlings <remi.p@hotmail.com>
2020-05-07 09:24:19 +0200

    Add project files.

---
+++ NoviSDP2/DbTestContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SD2.Models;
+
+namespace SD2
+{
+    public class DbTestContext : DbContext
+    {
+        public DbTestContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ NoviSDP2/Repository/StudentRepository.cs
@@ -0,0 +1,23 @@
+﻿using SD2.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SD2.Models;
+
+namespace SD2.Repository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly DbTestContext _context;
+
+        public StudentRepository(DbTestContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Student> GetAll()
+        {
+            return _context.Students;
+        }
+    }
+}
